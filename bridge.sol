74f05f402b2b9699c03cff4995f2338fcbc3922a
Kildos3212 <dropwork89@gmail.com>
2023-06-19 14:00:02 +0200

    Add support for ERC-721A tokens

--- bridge.sol
+++ bridge.sol
@@ -2,3 +2,4 @@
 // Integrate Chainlink oracles for price data
 // Implement liquidity pool staking mechanism
 // Fix issues in multi-token smart contracts
+// Implement adaptive block size for Ethereum
