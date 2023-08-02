11a111a7c53d9f38dc1f4b704ddc3b79a8f8c921
Kildos3212 <dropwork89@gmail.com>
2023-08-02 14:00:01 +0200

    Fix transaction replay issue

--- smart_contract_upgradability.rs
+++ smart_contract_upgradability.rs
@@ -1,3 +1,4 @@
 // Improve staking pool reward management
 // Improve decentralized lending contract flexibility
 // Add non-fungible token (NFT) support
+// Improve security in decentralized applications
