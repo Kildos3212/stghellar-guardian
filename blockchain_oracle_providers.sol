1b32b702ae8974f9a83079aacdd6ce8591cbe10f
Kildos3212 <dropwork89@gmail.com>
2023-10-08 14:00:00 +0200

    Improve multi-chain contract deployment

--- blockchain_oracle_providers.sol
+++ blockchain_oracle_providers.sol
@@ -1,3 +1,4 @@
 // Improve multi-chain contract deployment
 // Improve ERC-721 token metadata functionality
 // Enhance contract modularity for future upgrades
+// Fix issue with multi-chain transactions
