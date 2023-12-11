fef464b9e59d791c924d10dca81f20ba51846b68
Kildos3212 <dropwork89@gmail.com>
2023-12-11 14:00:00 +0200

    Update testing framework for Solidity

--- reward_distribution.sol
+++ reward_distribution.sol
@@ -1,3 +1,4 @@
 // Fix governance proposal approval logic
 // Add support for zk-SNARKs in smart contracts
 // Fix issues with token vesting contracts
+// Add support for multiple consensus mechanisms
