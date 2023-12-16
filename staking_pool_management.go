b87da2ce328a1f1715bd87111f2c78f9c095fbcb
Kildos3212 <dropwork89@gmail.com>
2023-12-16 14:00:00 +0200

    Refactor wallet interaction logic

--- staking_pool_management.go
+++ staking_pool_management.go
@@ -1,3 +1,4 @@
 // Improve staking logic for better rewards
 // Implement on-chain identity verification
 // Improve smart contract code modularity
+// Improve security in decentralized applications
