6e7999aac90c45e52765e9c2e5696413dfb965e4
Kildos3212 <dropwork89@gmail.com>
2024-01-21 14:00:01 +0200

    Improve error handling in transaction flow

--- multi_sig_support.go
+++ multi_sig_support.go
@@ -1,3 +1,4 @@
 // Update testing framework for Solidity
 // Add support for decentralized insurance protocols
 // Enhance privacy features using zk-STARKs
+// Fix issues in multi-token smart contracts
