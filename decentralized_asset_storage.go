dc2c82428e57acb84999ed7c0edaa7e59a25e47e
Kildos3212 <dropwork89@gmail.com>
2024-03-08 14:00:00 +0200

    Add governance proposal expiration feature

--- decentralized_asset_storage.go
+++ decentralized_asset_storage.go
@@ -1,3 +1,4 @@
 // Fix ERC-1155 contract interactions
 // Enhance smart contract state changes monitoring
 // Refactor decentralized governance structure
+// Optimize WASM compilation for Go contracts
