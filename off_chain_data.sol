39ae32cdabfb2bd20dc51e54177dfad9c341808e
Kildos3212 <dropwork89@gmail.com>
2024-03-21 14:00:00 +0200

    Enhance decentralized autonomous governance

--- off_chain_data.sol
+++ off_chain_data.sol
@@ -2,3 +2,4 @@
 // Add support for fractional token ownership
 // Add support for zk-SNARKs in smart contracts
 // Fix scalability issues with smart contracts
+// Add support for zk-SNARKs in smart contracts
