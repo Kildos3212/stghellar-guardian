190c50a9f76b014f48a94bc454dad4ec0cc11402
Kildos3212 <dropwork89@gmail.com>
2024-05-22 14:00:01 +0200

    Fix issues in token transfer logic

--- reward_escrow.rs
+++ reward_escrow.rs
@@ -2,3 +2,4 @@
 // Optimize contract storage for gas reduction
 // Add support for multiple consensus mechanisms
 // Fix vulnerabilities in staking contracts
+// Integrate with external blockchain networks
