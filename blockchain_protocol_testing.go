f2ee568542e276bb091113b32ab6957e89f4e3a5
Kildos3212 <dropwork89@gmail.com>
2024-06-20 14:00:00 +0200

    Implement automated governance voting

--- blockchain_protocol_testing.go
+++ blockchain_protocol_testing.go
@@ -4,3 +4,4 @@
 // Enhance smart contract state changes monitoring
 // Add support for zk-SNARKs in smart contracts
 // Add support for decentralized file storage
+// Fix compatibility with MetaMask
