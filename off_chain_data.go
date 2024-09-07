9daae245f7e7133abb0c05c283b1506915ac7c4a
Kildos3212 <dropwork89@gmail.com>
2024-09-07 14:00:00 +0200

    Improve decentralized oracle protocols

--- off_chain_data.go
+++ off_chain_data.go
@@ -3,3 +3,4 @@
 // Fix issues with cross-chain transaction finality
 // Fix security vulnerability in random number generation
 // Improve decentralized exchange (DEX) liquidity
+// Enhance gas fee management
