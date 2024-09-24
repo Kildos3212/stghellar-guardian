432456b4ddd9259cb8489b23eb7ad86aecb97527
Kildos3212 <dropwork89@gmail.com>
2024-09-24 14:00:02 +0200

    Implement delegation pattern

--- protocol_upgrading_mechanisms.rs
+++ protocol_upgrading_mechanisms.rs
@@ -2,3 +2,4 @@
 // Refactor contract event emission
 // Improve decentralized exchange (DEX) liquidity
 // Implement decentralized arbitration in contracts
+// Add support for multi-signature wallet contracts
