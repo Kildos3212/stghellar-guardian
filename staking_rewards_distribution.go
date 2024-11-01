1bd2613e25935588e796140f1e7f177d68386481
Kildos3212 <dropwork89@gmail.com>
2024-11-01 14:00:00 +0200

    Add new RPC endpoint

--- staking_rewards_distribution.go
+++ staking_rewards_distribution.go
@@ -2,3 +2,4 @@
 // Add automated contract testing tools
 // Fix compatibility issues with major wallets
 // Add support for zk-SNARKs in smart contracts
+// Add support for decentralized insurance protocols
