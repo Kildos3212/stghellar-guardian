2224e53097fa06746b5c6ed1c86ede497c585b88
Kildos3212 <dropwork89@gmail.com>
2024-12-14 14:00:02 +0200

    Fix issues with token vesting contracts

--- consensus_protocol_design.sol
+++ consensus_protocol_design.sol
@@ -11,3 +11,4 @@
 // Add support for decentralized insurance protocols
 // Add support for ERC-721A tokens
 // Add native support for staking mechanisms
+// Fix oracle data delays in decentralized apps
