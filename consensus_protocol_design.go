bf025de9fafe0854ab7e35a7d1912cb0d292529b
Kildos3212 <dropwork89@gmail.com>
2025-01-09 14:00:02 +0200

    Improve token burning mechanism

--- consensus_protocol_design.go
+++ consensus_protocol_design.go
@@ -1,3 +1,4 @@
 // Integrate with Ethereum Layer-2 solutions
 // Refactor storage layout for better efficiency
 // Integrate zk-SNARKs for enhanced privacy
+// Add support for zk-SNARKs in smart contracts
